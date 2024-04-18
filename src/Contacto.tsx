8ebda461ec0671b1a22bbfc804d1df6d04b07056
Marssalvador <mars.salvadorcardoso@gmail.com>
2024-04-18 10:55:14 +0200

    Contacto

--- src/Contacto.tsx
+++ src/Contacto.tsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { Button } from 'primereact/button';
 
 export const Contacto = () => {
-  return (
-    <div>Contacto</div>
-  )
-}
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const mailtoLink = `mailto:stafko@gmail.com?subject=Contacto desde formulario&body=Nombre: ${name}%0DEmail: ${email}%0DMensaje: ${message}`;
+        window.location.href = mailtoLink;
+    };
+
+    const handleShowAlert = () => {
+        alert('¡Mensaje enviado con éxito!');
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="p-fluid" style={{background:'linear-gradient(to right, #a18205, #ff9800)', borderRadius:'20px'}}>
+            <h1>Contáctanos</h1><br />
+            <div className="p-field p-grid">
+                <label htmlFor="name" className="p-col-fixed" style={{ width: '100px' }}>Nombre</label>
+                <div className="p-col">
+                    <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} className="p-inputtext" required />
+                </div>
+            </div><br />
+            <div className="p-field p-grid">
+                <label htmlFor="email" className="p-col-fixed" style={{ width: '100px' }}>Email</label>
+                <div className="p-col">
+                    <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="p-inputtext" required />
+                </div>
+            </div><br />
+            <div className="p-field p-grid">
+                <label htmlFor="message" className="p-col-fixed" style={{ width: '100px' }}>Motivo de contacto</label>
+                <div className="p-col">
+                    <InputTextarea id="message" value={message} onChange={(e) => setMessage(e.target.value)} required rows={5} className="p-inputtext" />
+                </div>
+            </div><br />
+
+            <div className="p-field">
+                <Button onClick={handleShowAlert} label="Enviar mensaje" icon="pi pi-send" />
+            </div><br />
+            
+            <div className="p-field">
+                <Button type="submit" label="Prefiero mandar email" icon="pi pi-info-circle" />
+            </div>
+
+        </form>
+    );
+};
