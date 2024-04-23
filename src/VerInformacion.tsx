34f0c1be78068c79063a04de90330d05a5ae27ad
Marssalvador <mars.salvadorcardoso@gmail.com>
2024-04-23 09:16:01 +0200

    Mostrar el nombre del Staff en el modal

--- src/VerInformacion.tsx
+++ src/VerInformacion.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './VerInformacion.css';
+import axios from 'axios';
 
 interface Proyecto {
   id: number;
@@ -16,23 +17,28 @@ interface Props {
   onClose: () => void;
 }
 
-
-//modificacion
 const VerInformacion: React.FC<Props> = ({ proyecto, onClose }) => {
-  const [nombreStaff, setNombreStaff] = useState<string>('Cargando...');
+  const [nombreStaff, setNombreStaff] = useState("");
 
   useEffect(() => {
-    const fetchNombreStaff = async () => {
-     
-        const response = await fetch(`/usuarios/datos/${proyecto.id_staff}`);
-        const data = await response.json();
-        setNombreStaff(data.nombre);
-  
+    const obtenerUsuarios = async () => {
+      try {
+        const response = await axios.get(`http://localhost:4000/usuarios/datos/${proyecto.id_staff}`);
+        if (response) {
+          setNombreStaff(response.data); //sacamos solo el nombre del Staff
+        } else {
+          setNombreStaff(""); 
+        }
+      } catch (error) {
+        console.error('Error al obtener nombre usuario:', error);
+      }
     };
+  
+    obtenerUsuarios();
+  }, []);
 
-    fetchNombreStaff();
-  }, [proyecto.id_staff]);
 
+  //formateamos la fecha para que sea formato español
   const formatFecha = (fecha: string): string => {
     const fechaObj = new Date(fecha);
     const dia = fechaObj.getDate();
@@ -40,6 +46,7 @@ const VerInformacion: React.FC<Props> = ({ proyecto, onClose }) => {
     const año = fechaObj.getFullYear();
     return `${dia}/${mes}/${año}`;
   };
+
 
   return (
     <div className="modal">
@@ -59,9 +66,6 @@ const VerInformacion: React.FC<Props> = ({ proyecto, onClose }) => {
           <br />
           <label>Fecha de fin:</label>
           <input type="text" value={formatFecha(proyecto.fecha_fin)} readOnly />
-          <br />
-          <label>ID del Staff:</label>
-          <input type="text" value={proyecto.id_staff} readOnly />
           <br />
           <label>Nombre del Staff:</label>
           <input type="text" value={nombreStaff} readOnly />
@@ -73,77 +77,3 @@ const VerInformacion: React.FC<Props> = ({ proyecto, onClose }) => {
 };
 
 export default VerInformacion;
-
-
-
-
-
-/*
-
-
-import React from 'react';
-import './VerInformacion.css'
-
-interface Proyecto {
-    id: number;
-    nombre: string;
-    descripcion: string;
-    cuantia: string;
-    fecha_inicio: string;
-    fecha_fin: string;
-    id_staff: string;
-  }
-
-interface Props {
-  proyecto: Proyecto;
-  onClose: () => void;
-}
-
-const VerInformacion: React.FC<Props> = ({ proyecto, onClose }) => {
-
-    const formatFecha = (fecha: string): string => {
-        // Convertir la cadena de fecha a objeto Date
-        const fechaObj = new Date(fecha);
-        // Obtener el día, mes y año
-        const dia = fechaObj.getDate();
-        const mes = fechaObj.getMonth() + 1; // Sumar 1 al mes ya que los meses van de 0 a 11
-        const año = fechaObj.getFullYear();
-        // Devolver la fecha en el formato d/m/y
-        return `${dia}/${mes}/${año}`;
-      };
-
-  return (
-    <div className="modal">
-      <div className="modal-content flex flex-col items-center justify-center bg-gradient-to-r from-orange-200 p-5 rounded-lg shadow-lg mb-6 max-w-md w-full">
-        <div>
-          <label>Nombre:</label>
-          <input type="text" value={proyecto.nombre} readOnly />
-          <br />
-          <label>Descripción:</label>
-          <textarea value={proyecto.descripcion} readOnly />
-          <br />
-          <label>Cuantía:</label>
-          <input type="text" value={proyecto.cuantia} readOnly />
-          <br />
-          <label>Fecha de inicio:</label>
-          <input type="text" value={formatFecha(proyecto.fecha_inicio)} readOnly />
-          <br />
-          <label>Fecha de fin:</label>
-          <input type="text" value={formatFecha(proyecto.fecha_fin)} readOnly />
-          <br />
-          <label>ID del Staff:</label>
-          <input type="text" value={proyecto.id_staff} readOnly />
-        </div>
-        <button className="close-modal" onClick={onClose}>Cerrar</button>
-      </div>
-    </div>
-  );
-};
-
-export default VerInformacion;
-
-
-
-
-
-*/
