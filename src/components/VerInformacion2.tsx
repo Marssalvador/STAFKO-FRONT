5c899bfb24956406edea15a1f56ed9f787d137fc
Marssalvador <mars.salvadorcardoso@gmail.com>
2024-05-07 11:11:46 +0200

    Mostrar proyecto solo en clientes

--- src/components/VerInformacion2.tsx
+++ src/components/VerInformacion2.tsx
@@ -10,15 +10,13 @@ interface Usuarios {
   id_proyecto?: number | null | undefined; // Permitir undefined
 }
 
-
 interface Props {
   usuario: Usuarios;
   onClose: () => void;
 }
 
 const VerInformacion2: React.FC<Props> = ({ usuario, onClose }) => {
-
-  //formateamos la fecha para que tenga formato español
+  // Formateamos la fecha para que tenga formato español
   const formatFecha = (fecha: string): string => {
     const fechaObj = new Date(fecha);
     const dia = fechaObj.getDate();
@@ -27,7 +25,7 @@ const VerInformacion2: React.FC<Props> = ({ usuario, onClose }) => {
     return `${dia}/${mes}/${año}`;
   };
 
-  //mostramos los datos en un modal
+  // Mostramos los datos en un modal
   return (
     <div className="modal">
       <div className="modal-content flex flex-col items-center justify-center bg-gradient-to-r from-orange-200 p-5 rounded-lg shadow-lg mb-6 max-w-md w-full">
@@ -35,24 +33,29 @@ const VerInformacion2: React.FC<Props> = ({ usuario, onClose }) => {
           <label>Nombre:</label>
           <input type="text" value={usuario.nombre} readOnly />
           <br />
+
           <label>Apellido:</label>
           <textarea value={usuario.apellido} readOnly />
           <br />
+
           <label>Usuario:</label>
           <input type="text" value={usuario.username} readOnly />
           <br />
+
           <label>Telefono:</label>
           <textarea value={usuario.telefono} readOnly />
           <br />
+          
           <label>Fecha de nacimiento:</label>
           <input type="text" value={formatFecha(usuario.fecha_nacimiento)} readOnly />
           <br />
-          <div>
-          <label>Id proyecto:</label>
-          <input type="text" value={usuario.id_proyecto !== null ? usuario.id_proyecto : ''} readOnly />
 
-        </div>
-
+          {usuario.id_proyecto !== null && usuario.id_proyecto !== undefined && (
+            <div>
+              <label>Id proyecto:</label>
+              <input type="text" value={usuario.id_proyecto} readOnly />
+            </div>
+          )}
         </div>
         <button className="close-modal" onClick={onClose}>Cerrar</button>
       </div>
