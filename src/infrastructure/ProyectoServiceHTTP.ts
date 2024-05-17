b100ad720187b187a8157f143dfe7dfccc8d7d7f
Marssalvador <mars.salvadorcardoso@gmail.com>
2024-05-17 13:27:55 +0200

    Mostrar nombre Staffs en Añadir Proyecto Directus

--- src/infrastructure/ProyectoServiceHTTP.ts
+++ src/infrastructure/ProyectoServiceHTTP.ts
@@ -18,15 +18,25 @@ export class ProyectoServiceHTTP {
   }
 
   async obtenerStaffs(): Promise<Staff[]> {
-    const response = await fetch('http://localhost:4000/usuarios/ids-nombres');
+    const response = await fetch('http://localhost:8055/items/usuarios?fields=nombre&fields=id');
     if (!response.ok) {
       throw new Error('Error al obtener usuarios');
     }
     const data = await response.json();
-    if (data && Array.isArray(data.rows)) {
-      return data.rows;
+    
+    // Verificamos si la propiedad "data" existe en la respuesta
+    if (data && Array.isArray(data.data)) {
+      return data.data.map((usuario: any) => ({
+        id: usuario.id,
+        nombre: usuario.nombre,
+        apellido: "",
+        username: "",
+        telefono: "",
+        fecha_nacimiento: "",
+        rol: "staff"
+      }));
     } else {
-      throw new Error('La respuesta de la API no contiene un arreglo de usuarios en la propiedad "rows"');
+      throw new Error('La respuesta de la API no contiene un arreglo de usuarios en la propiedad "data"');
     }
   }
 }
